35cd18c387baeab55cfa8dfa33fe41905333b1db
Bernard Šetinc <besetinc@vsite.hr>
2019-12-05 19:38:53 +0100

    Resolve methods with default args

--- PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
+++ PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
@@ -4,21 +4,19 @@ namespace Vsite.CSharp.Metode
 {
     class PreopterećenjePodrazumijevaniArg
     {
-        // TODO:010 Pokrenuti program i provjeriti ispise.
         public static int Zbroji(int x = 1, int y = 2, int z = 3)
         {
             return x + y + z;
         }
 
-        // TODO:011 Preopteretiti funkciju Zbroji novom funkcijom public static int Zbroji(int x, int y) koja će vratiti zbroj argumenata. 
+        public static int Zbroji(int x, int y)
+        {
+            return x + y;
+        }
 
-
-        // TODO:012 Ponovno pokrenuti programa te usporediti ispise.
-
-        // TODO:013 Provjeriti što bi se dogodilo ako bismo drugoj metodi definirali y s podrazumijevanim argumentom.
-
-        // TODO:014 Pokrenuti i provjeriti testove (3 testa "PreopterećenjePodrazumijevaniArg_..." moraju proći).
-
+        
+        
+        
 
         static void Main(string[] args)
         {
