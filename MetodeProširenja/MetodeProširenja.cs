68d83b177c154fbd0160aa7e7afafb596ee736e9
Auro Mazzi <auro.mazzi@gmail.com>
2019-12-05 21:30:00 +0100

    Resolve expansion methods

--- MetodeProširenja/MetodeProširenja.cs
+++ MetodeProširenja/MetodeProširenja.cs
@@ -3,7 +3,13 @@
 namespace Vsite.CSharp.Metode
 {
     // TODO:120 Dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
-
+    static class MojeMetodeProširenja
+    {
+        public static int BrojRiječi(this string s)
+        {
+            return s.Split(new char[] { ' ', '.', '-', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
 
 
     class MetodeProširenja
@@ -11,7 +17,7 @@ namespace Vsite.CSharp.Metode
         public static int DajBrojRiječi(string tekst)
         {
             // TODO:121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
-            return 0;
+            return tekst.BrojRiječi();
         }
 
         // TODO:122 Pokrenuti i provjeriti testove (4 testa "MetodaProširenja_..." moraju proći).
