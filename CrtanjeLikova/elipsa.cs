13b5a0f585bcc8c93c2a6f922c1275bb4c3b926e
Marko Vitković <mavitkov@vsite.hr>
2019-12-05 21:10:25 +0100

    done, 09 10 11 homework

---
+++ CrtanjeLikova/elipsa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.CSharp.Metode.GeometrijskiLikovi;
+
+namespace Vsite.CSharp.Metode.GeometrijskiLikovi;
+{
+	class elipsa : GeometrijskiLik
+	{
+		public elipsa(float x, float y, float širina, float visina)
+		{
+			this.x = x;
+			this.y = y;
+			this.širina = širina;
+			this.visina = visina;
+		}
+
+		public override void Nacrtaj(Graphics g)
+		{
+			g.DrawEllipse(Pens.Crimson, x, y, širina, visina);
+		}
+
+		public override void Pomakni(float deltaX, float deltaY)
+		{
+			x+=
+			
+		}
+
+		public override void Uvećaj(float faktor)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
