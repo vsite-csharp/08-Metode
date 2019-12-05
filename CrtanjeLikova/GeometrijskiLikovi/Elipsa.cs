92b872127b8e2bc9ddd9e85684d5a26f0c0496f1
Domagoj Salkić <48859348+domagojsalkic@users.noreply.github.com>
2019-12-05 21:19:30 +0100

    Resolve graphical objects

--- CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -1,17 +1,21 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Vsite.CSharp.Metode.GeometrijskiLikovi
 {
-    class Pravokutnik : GeometrijskiLik
+    class Elipsa : GeometrijskiLik
     {
-        public Pravokutnik(float x, float y, float širina, float visina)
+        public Elipsa(float x, float y, float širina, float visina)
         {
             this.x = x;
             this.y = y;
             this.širina = širina;
             this.visina = visina;
         }
-
 
         public override void Pomakni(float deltaX, float deltaY)
         {
@@ -27,12 +31,12 @@ namespace Vsite.CSharp.Metode.GeometrijskiLikovi
 
         protected override void Popuni(Graphics g)
         {
-            g.FillRectangle(Brushes.Gray, x, y, širina, visina);
+            g.FillEllipse(Brushes.DarkCyan, x,y,širina,visina);        
         }
 
         protected override void NacrtajOkvir(Graphics g)
         {
-            g.DrawRectangle(Pens.Black, x, y, širina, visina);
+            g.DrawEllipse(Pens.Black, x, y, širina, visina);     
         }
 
         private float x;
