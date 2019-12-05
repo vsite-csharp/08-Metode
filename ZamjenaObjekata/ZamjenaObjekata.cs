5227d00c6b511ebb80dba59cdd838d93bd224e31
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2019-12-05 20:26:27 +0100

    Resolve generics swapping

--- ZamjenaObjekata/ZamjenaObjekata.cs
+++ ZamjenaObjekata/ZamjenaObjekata.cs
@@ -40,7 +40,6 @@ namespace Vsite.CSharp.Metode
 
           
             
-            // TODO:062 Preopteretiti metodu Zamijeni tako da umjesto tipa string prima tip object i tako podržava zamjenu bilo kojeg tipa objekta. Dodati poziv te metode za 2 argumenta tipa int te provjeriti funkcionalnost.
             object i1 = (object)prvi;
             object i2 = (object)drugi;
             Zamijeni(ref i1, ref i2);
@@ -51,7 +50,6 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("drugi = '{0}'", drugi);
         }
 
-        // TODO:063 Pokrenuti i provjeriti testove (2 testa "ZamjenaObjekata_..." moraju proći)
 
         static void Main(string[] args)
         {
