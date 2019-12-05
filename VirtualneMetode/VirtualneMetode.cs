ef7d7a01662c83071f88b26e35557a5866e6907b
Marko Vitković <mavitkov@vsite.hr>
2019-12-05 20:47:43 +0100

    done 08

--- VirtualneMetode/VirtualneMetode.cs
+++ VirtualneMetode/VirtualneMetode.cs
@@ -15,18 +15,29 @@ namespace Vsite.CSharp.Metode
         }
     }
 
-    // TODO:081 U klase Izvedena1 i Izvedena2 dodajte javno dostupne metode IspišiImeObično koje će ispisati "Izvedena1 Obično", odnosno "Izvedena2 Obično". Pokrenite program i usporedite ispise s prijašnjima.
-
-    // TODO:082 U klase Izvedena1 i Izvedena2 dodajte javno dostupne metode IspišiImeVirtualno koje će ispisati "Izvedena1 Virtualno", odnosno "Izvedena2 Virtualno". Pokrenite program i usporedite ispise s prijašnjima.
     class Izvedena1 : Bazna
     {
-
-    }
+		public new void IspišiImeObično()
+		{
+			Console.WriteLine("Izvedena1 Obično");
+		}
+		public override void IspišiImeVirtualno()
+		{
+			Console.WriteLine("Izvedena1 Virtualno");
+		}
+	}
 
     class Izvedena2 : Bazna
     {
-
-    }
+		public new void IspišiImeObično()
+		{
+			Console.WriteLine("Izvedena2 Obično");
+		}
+		public override void IspišiImeVirtualno()
+		{
+			Console.WriteLine("Izvedena2 Virtualno");
+		}
+	}
 
     class VirtualneMetode
     {
@@ -71,7 +82,7 @@ namespace Vsite.CSharp.Metode
 
         static void Main(string[] args)
         {
-            // TODO:080 Pokrenite program i provjerite što će se ispisati.
+            
 
             Console.WriteLine("Zasebni pozivi nevirtualnih metoda:");
             ZasebniPoziviNevirtualnihMetoda();
