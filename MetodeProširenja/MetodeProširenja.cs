b6855e7d5c1d3722d65e61b6be18732a66854c2f
Krunoslav Bučar <krbuki@gmail.com>
2019-12-05 21:27:57 +0100

    Resolve extends methods

--- MetodeProširenja/MetodeProširenja.cs
+++ MetodeProširenja/MetodeProširenja.cs
@@ -3,7 +3,14 @@
 namespace Vsite.CSharp.Metode
 {
     // TODO:120 Dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
-
+    static class MojeMetodeProširenja
+    {
+        public static int BrojRiječi(this string s)
+        {
+            return s.Split(new char[] { ' ', '.', '.',  '?', '!', },StringSplitOptions.RemoveEmptyEntries).Length;
+            //return 0;
+        }
+    }
 
 
     class MetodeProširenja
@@ -11,7 +18,12 @@ namespace Vsite.CSharp.Metode
         public static int DajBrojRiječi(string tekst)
         {
             // TODO:121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
-            return 0;
+
+            return tekst.BrojRiječi();
+                    /*U 'Kako je poceo rat na mom otoku?' ima 7 rijeci.
+        U 'Brojim: jedan, dva, tri! Tko se nije skrio...' ima 8 rijeci.
+        GOTOVO!!!*/
+                    //return 0;
         }
 
         // TODO:122 Pokrenuti i provjeriti testove (4 testa "MetodaProširenja_..." moraju proći).
