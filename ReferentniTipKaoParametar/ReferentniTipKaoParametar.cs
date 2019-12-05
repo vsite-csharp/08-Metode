cfd1fd90d16e3a0db71355a95a2eb20173ee6c6d
Ivan <ivan.zoric98@gmail.com>
2019-12-05 19:53:04 +0100

    Resolve reference type as parameter

--- ReferentniTipKaoParametar/ReferentniTipKaoParametar.cs
+++ ReferentniTipKaoParametar/ReferentniTipKaoParametar.cs
@@ -27,19 +27,15 @@ namespace Vsite.CSharp.Metode
             Osoba janko = new Osoba("Janko", 1);
             Console.WriteLine(janko);
 
-            // TODO:030 Dodati poziv metode ZamijeniOsobuMirkom s prosljeđenom osobom janko i ispisati osobu nakon poziva te metode.
-
-
-            // TODO:031 Pokrenuti program i obrazložiti ispis.
+            ZamijeniOsobuMirkom(janko);
+            Console.WriteLine(janko);
 
             Console.WriteLine();
 
-            // TODO:032 Dodati poziv metode PromijeniIme s prosljeđenom osobom janko i novim imenom "Pero" i ispisati osobu nakon poziva te metode.
+            PromijeniIme(janko, "Pero");
+                Console.WriteLine(janko);
 
 
-            // TODO:033 Pokrenuti program i obrazložiti ispis.
-
-            // TODO:034 Pokrenuti i provjeriti testove (test ReferentiTipKaoArgument_NakonIzlaskaIzMetodeObjektJeNepromijenjen treba proći)
         }
 
         static void Main(string[] args)
