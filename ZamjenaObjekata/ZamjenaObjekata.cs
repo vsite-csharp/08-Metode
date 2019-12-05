67aeba47ad64c82f56e5a375750a4f34ed24b8c2
KaMiletic <56977458+KaMiletic@users.noreply.github.com>
2019-12-05 20:22:13 +0100

    resolve object swaping

--- ZamjenaObjekata/ZamjenaObjekata.cs
+++ ZamjenaObjekata/ZamjenaObjekata.cs
@@ -4,12 +4,17 @@ namespace Vsite.CSharp.Metode
 {
     class ZamjenaObjekata
     {
-        // TODO:060 Pokrenuti program i provjeriti ispis.
 
-        // TODO:061 Promijeniti metodu tako da se zamjena odrazi u pozivajućem kodu.
-        static void Zamijeni(string s1, string s2)
+        static void Zamijeni(ref string s1,ref string s2)
         {
             string temp = s2;
+            s2 = s1;
+            s1 = temp;
+        }
+
+        static void Zamijeni(ref object s1, ref object s2)
+        {
+            var temp = s2;
             s2 = s1;
             s1 = temp;
         }
@@ -20,7 +25,7 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
 
-            Zamijeni(prvi, drugi);
+            Zamijeni(ref prvi, ref drugi);
 
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
@@ -33,14 +38,17 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
 
-            // TODO:062 Preopteretiti metodu Zamijeni tako da umjesto tipa string prima tip object i tako podržava zamjenu bilo kojeg tipa objekta. Dodati poziv te metode za 2 argumenta tipa int te provjeriti funkcionalnost.
 
+            
+
+            object i1 = (object)prvi;
+            object i2 = (object)drugi;
+            Zamijeni(ref i1, ref i2);
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
         }
 
-        // TODO:063 Pokrenuti i provjeriti testove (2 testa "ZamjenaObjekata_..." moraju proći)
 
         static void Main(string[] args)
         {
