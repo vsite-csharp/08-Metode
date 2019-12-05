2df05a718f1234d207ccc7c1cfddc818288e0554
Domagoj Krmpotić <domagojkrmpoti3@gmail.com>
2019-12-05 22:45:30 +0100

    Resolve generic switch

--- ZamjenaObjekata/ZamjenaObjekata.cs
+++ ZamjenaObjekata/ZamjenaObjekata.cs
@@ -36,7 +36,6 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("prvi = '{0}'",  prvi);
             Console.WriteLine("drugi = '{0}'",  drugi);
 
-            // TODO:062 Preopteretiti metodu Zamijeni tako da umjesto tipa string prima tip object i tako podržava zamjenu bilo kojeg tipa objekta. Dodati poziv te metode za 2 argumenta tipa int te provjeriti funkcionalnost.
             object i1=(object)prvi;
             object i2=(object)drugi;
             Zamijeni(ref i1,ref i2);
@@ -47,7 +46,6 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("drugi = '{0}'", drugi);
         }
 
-        // TODO:063 Pokrenuti i provjeriti testove (2 testa "ZamjenaObjekata_..." moraju proći)
 
         static void Main(string[] args)
         {
