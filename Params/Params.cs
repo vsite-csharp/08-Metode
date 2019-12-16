9d629cdab070a57d578cf25f5c00fb50c5dce6b7
Marko Bohač <mabohac@vsite.hr>
2019-12-16 16:25:45 +0100

    Resolved params

--- Params/Params.cs
+++ Params/Params.cs
@@ -5,17 +5,20 @@ namespace Vsite.CSharp.Metode
 
     class Params
     {
-        // TODO:110 Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
+        // Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
         public static double Zbroji(params double[] pribrojnici)
         {
-            return 0.0;
+            double sum = 0;
+            foreach (double d in pribrojnici)
+                sum += d;
+            return sum;
         }
 
-        // TODO:111 Pokrenuti i provjeriti testove (3 testa "Params_..." moraju proći).
+        // Pokrenuti i provjeriti testove (3 testa "Params_..." moraju proći).
 
         static void Main(string[] args)
         {
-            double zbroj = Zbroji(1, 2, 3, 4);
+            double zbroj = Zbroji(1, 2, 3, 4, 4);
             Console.WriteLine(zbroj);
 
             Console.WriteLine(Zbroji(3.2, 4.3, 7.5));
