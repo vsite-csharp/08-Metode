6f273913069df6681364df9989fe17ed0102ac34
Franjo Gregorić <franjo.gregoric@triple-innovations.com>
2020-12-10 20:23:20 +0100

    GenerickaZamjena

--- GeneričkaZamjena/GeneričkaZamjena.cs
+++ GeneričkaZamjena/GeneričkaZamjena.cs
@@ -5,7 +5,12 @@ namespace Vsite.CSharp.Metode
     class GeneričkaZamjena
     {
         // TODO:070 Definirati javno dostupnu statičku generičku metodu Zamijeni<T>, koja će biti parametrizirana tipom T i moći raditi tipski sigurnu zamjenu bilo koja dva objekta istog tipa.
-
+        public static void Zamijeni<T>(ref T prvi, ref T drugi)
+        {
+            T priv = prvi;
+            prvi = drugi;
+            drugi = priv;
+        }
 
         private static void IspišiPar(object o1, object o2)
         {
@@ -18,7 +23,7 @@ namespace Vsite.CSharp.Metode
             int i2 = 10;
             IspišiPar(i1, i2);
             // TODO:071 Dodati poziv metode Zamijeni<T> za i1 i i2, pokrenuti program i provjeriti ispis.
-
+            Zamijeni<int>(ref i1, ref i2);
             IspišiPar(i1, i2);
 
             Console.WriteLine();
@@ -27,7 +32,7 @@ namespace Vsite.CSharp.Metode
             double d2 = 4.56;
             IspišiPar(d1, d2);
             // TODO:072 Dodati poziv metode Zamijeni<T> za d1 i d2, pokrenuti program i provjeriti ispis.
-
+            Zamijeni(ref d1, ref d2);
             IspišiPar(d1, d2);
 
             Console.WriteLine();
@@ -36,7 +41,7 @@ namespace Vsite.CSharp.Metode
             string s2 = "Marica";
             IspišiPar(s1, s2);
             // TODO:073 Dodati poziv metode Zamijeni<T> za s1 i s2, pokrenuti program i provjeriti ispis.
-
+            Zamijeni(ref s1, ref s2);
             IspišiPar(s1, s2);
 
             // TODO:074 Pokrenuti i provjeriti testove (3 testa u grupi TestGeneričkeZamjene moraju proći).
