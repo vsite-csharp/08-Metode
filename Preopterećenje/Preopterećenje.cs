19c3fff8e3d528ddad61d9e13ec8c21dcb134b99
Ivan Ceranac <ivcerana@vsite.hr>
2020-12-10 19:33:46 +0100

    Resolve preopterecenjePodrazumjevaniArg

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -4,13 +4,15 @@ namespace Vsite.CSharp.Metode
 {
     class Preopterećenje
     {
-        // TODO:000 Pokrenuti program i provjeriti ispis.
+        //  Pokrenuti program i provjeriti ispis.
         public static void MojaMetoda(double broj1, double broj2)
         {
             Console.WriteLine($"MojaMetoda(double {broj1}, double {broj2})");
         }
 
         // TODO:001 Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+
+
 
         // TODO:002 Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
 
