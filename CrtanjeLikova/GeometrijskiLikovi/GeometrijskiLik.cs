6cbfabf1e40b8ec7ad339e4835d53008a5d04b9c
Zvonimir Dražić <zvonimir.drazic@gmail.com>
2020-12-10 21:08:54 +0100

    Resolve shapes

--- CrtanjeLikova/GeometrijskiLikovi/GeometrijskiLik.cs
+++ CrtanjeLikova/GeometrijskiLikovi/GeometrijskiLik.cs
@@ -2,14 +2,14 @@
 {
     abstract class GeometrijskiLik
     {
-        // TODO:093 Dodati zaštićene apstraktne metode Popuni i NacrtajOkvir.
+        // TODO:093 DZ Dodati zaštićene apstraktne metode Popuni i NacrtajOkvir.
 
-        // TODO:094 Apstraktnu metodu Nacrtaj nadomjestiti nevirtualnom metodom koja će pozivati apstraktne metode Popuni i NacrtajOkvir.
+        // TODO:094 DZ Apstraktnu metodu Nacrtaj nadomjestiti nevirtualnom metodom koja će pozivati apstraktne metode Popuni i NacrtajOkvir.
         public abstract void Nacrtaj(System.Drawing.Graphics g);
 
-        // TODO:095 U izvedenim klasama ukloniti metodu Nacrtaj te implementirati metode Popuni i NacrtajOvir.
+        // TODO:095 DZ U izvedenim klasama ukloniti metodu Nacrtaj te implementirati metode Popuni i NacrtajOvir.
 
-        // TODO:096 Dodati u konstruktore boju okvira i boju popune te ih upotrijebiti za iscrtavanje.
+        // TODO:096 DZ Dodati u konstruktore boju okvira i boju popune te ih upotrijebiti za iscrtavanje.
 
         public abstract void Pomakni(float deltaX, float deltaY);
 
