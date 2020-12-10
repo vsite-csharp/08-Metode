d6e3132ffff03b29f65929d3d855d80cd24253df
Franjo Gregorić <franjo.gregoric@triple-innovations.com>
2020-12-10 20:05:51 +0100

    OutParametar

--- OutParametar/OutParametar.cs
+++ OutParametar/OutParametar.cs
@@ -5,7 +5,7 @@ namespace Vsite.CSharp.Metode
     class OutParametar
     {
         // TODO:050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
-        static void InicijalizirajBrojNaDeset(int x)
+        static void InicijalizirajBrojNaDeset(out int x)
         {
             x = 10;
         }
@@ -13,7 +13,7 @@ namespace Vsite.CSharp.Metode
         public static void PozivInicijalizacijeOutParametrom()
         {
             int x = 0;
-            InicijalizirajBrojNaDeset(x);
+            InicijalizirajBrojNaDeset(out x);
             Console.WriteLine($"Nakon InicijalizirajBrojNaDeset: {x}");
         }
 
@@ -23,6 +23,11 @@ namespace Vsite.CSharp.Metode
             {
                 Console.WriteLine("Upiši neki cijeli broj:");
                 // TODO:051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
+                string text = Console.ReadLine();
+                if (int.TryParse(text, out int rezultat))
+                {
+                    return rezultat;
+                }
 
                 return 0;
             }
@@ -34,7 +39,9 @@ namespace Vsite.CSharp.Metode
 
             PozivInicijalizacijeOutParametrom();
 
-            UčitavanjeCijelogBroja();
+            int a = UčitavanjeCijelogBroja();
+
+            Console.Write(a);
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
