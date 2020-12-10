f36df2e196b1d8dbf946c00ebbf34f7639ff22b1
Jasmin Husić <jhsc1988@gmail.com>
2020-12-10 21:08:53 +0100

    Resolve shapes

---
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Metode.GeometrijskiLikovi
+{
+    class Elipsa : GeometrijskiLik // Ctrl + . na podcrtani Elipsa -> Implement abstract class
+    {
+        private float x;
+        private float y;
+        private float širina;
+        private float visina;
+
+        public override void Nacrtaj(Graphics g)
+        {
+            g.DrawEllipse(Pens.Black, x, y, širina, visina);
+        }
+
+        public override void Pomakni(float deltaX, float deltaY)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Uvećaj(float faktor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
