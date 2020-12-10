b588e90c3fb4cd7814551987c7c84b1b2180ae15
Ivan Ceranac <ivcerana@vsite.hr>
2020-12-10 21:23:46 +0100

    resolve extension methods

--- MetodeProširenja/MetodeProširenja.cs
+++ MetodeProširenja/MetodeProširenja.cs
@@ -2,20 +2,24 @@
 
 namespace Vsite.CSharp.Metode
 {
-    // TODO:120 U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
+    //  U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
     public static class MojaProširenja
     {
+        public static int BrojRiječi(this string str)
+        {
+            return str.Split(new char[] {' ', '.', ',', '?', ';', '!', ':' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
     }
 
     class MetodeProširenja
     {
         public static int DajBrojRiječi(string tekst)
         {
-            // TODO:121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
-            return 0;
+            //  Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
+            return tekst.BrojRiječi();
         }
 
-        // TODO:122 Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
+        //  Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
 
         static void Main(string[] args)
         {
