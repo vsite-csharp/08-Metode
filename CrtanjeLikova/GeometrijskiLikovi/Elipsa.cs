02670b3cfbc0651addbd4f1f45a8f1fec4855ade
Valentina Behin <72219767+ValentinaBehin@users.noreply.github.com>
2020-12-10 21:08:42 +0100

    Resolve shapes

---
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Metode.GeometrijskiLikovi
+{
+    class Elipsa : GeometrijskiLik
+    {
+        private float x;
+        private float y;
+        private float širina;
+        private float visina;
+        public Elipsa(float x, float y, float širina, float visina)
+        {
+            this.x = x;
+            this.y = y;
+            this.širina = širina;
+            this.visina = visina;
+        }
+        public override void Nacrtaj(Graphics g)
+        {
+            g.FillRectangle(Brushes.LightGray, x, y, širina, visina);
+            g.DrawEllipse(Pens.Black,x,y,širina,visina);
+        }
+
+        public override void Pomakni(float deltaX, float deltaY)
+        {
+            x += deltaX;
+            y += deltaY;
+        }
+
+        public override void Uvećaj(float faktor)
+        {
+            širina *= faktor;
+            visina *= faktor;
+        }
+    }
+}
