ecfd6eb2c8358abcbd411b4bca398dce4200438e
Anamarija Jozić <anjozic@vsite.hr>
2020-12-10 19:21:59 +0100

    resolve method overloading

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -4,17 +4,20 @@ namespace Vsite.CSharp.Metode
 {
     class Preopterećenje
     {
-        // TODO:000 Pokrenuti program i provjeriti ispis.
+        // Pokrenuti program i provjeriti ispis.
         public static void MojaMetoda(double broj1, double broj2)
         {
             Console.WriteLine($"MojaMetoda(double {broj1}, double {broj2})");
         }
 
-        // TODO:001 Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+        // Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+        public static void MojaMetoda(int broj1, double broj2)
+        {
+            Console.WriteLine($"MojaMetoda(int {broj1}, double {broj2})");
+        }
+        //  Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
 
-        // TODO:002 Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
-
-        // TODO:003 Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
+        //  Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
 
 
         static void Main(string[] args)
