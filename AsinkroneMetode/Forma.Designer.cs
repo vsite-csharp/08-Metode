811b86b1195d7aef4716eb6f3537b720ca284658
Paolo Stabile <pastabil@vsite.hr>
2020-12-12 21:47:40 +0100

    Resolve AsinkroneMetode

--- AsinkroneMetode/Forma.Designer.cs
+++ AsinkroneMetode/Forma.Designer.cs
@@ -40,9 +40,10 @@ namespace Vsite.CSharp.Metode
             // 
             // buttonSinkroni
             // 
-            this.buttonSinkroni.Location = new System.Drawing.Point(12, 50);
+            this.buttonSinkroni.Location = new System.Drawing.Point(16, 62);
+            this.buttonSinkroni.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSinkroni.Name = "buttonSinkroni";
-            this.buttonSinkroni.Size = new System.Drawing.Size(75, 23);
+            this.buttonSinkroni.Size = new System.Drawing.Size(100, 28);
             this.buttonSinkroni.TabIndex = 0;
             this.buttonSinkroni.Text = "Pritisni me...";
             this.buttonSinkroni.UseVisualStyleBackColor = true;
@@ -50,9 +51,10 @@ namespace Vsite.CSharp.Metode
             // 
             // buttonAsinkroni
             // 
-            this.buttonAsinkroni.Location = new System.Drawing.Point(134, 50);
+            this.buttonAsinkroni.Location = new System.Drawing.Point(179, 62);
+            this.buttonAsinkroni.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAsinkroni.Name = "buttonAsinkroni";
-            this.buttonAsinkroni.Size = new System.Drawing.Size(75, 23);
+            this.buttonAsinkroni.Size = new System.Drawing.Size(100, 28);
             this.buttonAsinkroni.TabIndex = 1;
             this.buttonAsinkroni.Text = "...i mene";
             this.buttonAsinkroni.UseVisualStyleBackColor = true;
@@ -60,23 +62,26 @@ namespace Vsite.CSharp.Metode
             // 
             // textBoxSinkronoVrijeme
             // 
-            this.textBoxSinkronoVrijeme.Location = new System.Drawing.Point(12, 12);
+            this.textBoxSinkronoVrijeme.Location = new System.Drawing.Point(16, 15);
+            this.textBoxSinkronoVrijeme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxSinkronoVrijeme.Name = "textBoxSinkronoVrijeme";
-            this.textBoxSinkronoVrijeme.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSinkronoVrijeme.Size = new System.Drawing.Size(132, 22);
             this.textBoxSinkronoVrijeme.TabIndex = 2;
             // 
             // textBoxAsinkronoVrijeme
             // 
-            this.textBoxAsinkronoVrijeme.Location = new System.Drawing.Point(134, 12);
+            this.textBoxAsinkronoVrijeme.Location = new System.Drawing.Point(179, 15);
+            this.textBoxAsinkronoVrijeme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxAsinkronoVrijeme.Name = "textBoxAsinkronoVrijeme";
-            this.textBoxAsinkronoVrijeme.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAsinkronoVrijeme.Size = new System.Drawing.Size(132, 22);
             this.textBoxAsinkronoVrijeme.TabIndex = 3;
             // 
             // buttonAsinkroni2
             // 
-            this.buttonAsinkroni2.Location = new System.Drawing.Point(257, 50);
+            this.buttonAsinkroni2.Location = new System.Drawing.Point(343, 62);
+            this.buttonAsinkroni2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAsinkroni2.Name = "buttonAsinkroni2";
-            this.buttonAsinkroni2.Size = new System.Drawing.Size(75, 23);
+            this.buttonAsinkroni2.Size = new System.Drawing.Size(100, 28);
             this.buttonAsinkroni2.TabIndex = 4;
             this.buttonAsinkroni2.Text = "...i mene";
             this.buttonAsinkroni2.UseVisualStyleBackColor = true;
@@ -84,25 +89,27 @@ namespace Vsite.CSharp.Metode
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(257, 88);
+            this.progressBar.Location = new System.Drawing.Point(343, 108);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(100, 14);
+            this.progressBar.Size = new System.Drawing.Size(133, 17);
             this.progressBar.TabIndex = 5;
             // 
             // textBoxAsinkroni2
             // 
-            this.textBoxAsinkroni2.Location = new System.Drawing.Point(258, 12);
+            this.textBoxAsinkroni2.Location = new System.Drawing.Point(344, 15);
+            this.textBoxAsinkroni2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxAsinkroni2.Name = "textBoxAsinkroni2";
             this.textBoxAsinkroni2.ReadOnly = true;
-            this.textBoxAsinkroni2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAsinkroni2.Size = new System.Drawing.Size(132, 22);
             this.textBoxAsinkroni2.TabIndex = 6;
             this.textBoxAsinkroni2.Text = "Pritisni tipku ispod";
             // 
             // Forma
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 114);
+            this.ClientSize = new System.Drawing.Size(493, 140);
             this.Controls.Add(this.textBoxAsinkroni2);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.buttonAsinkroni2);
@@ -110,6 +117,7 @@ namespace Vsite.CSharp.Metode
             this.Controls.Add(this.textBoxSinkronoVrijeme);
             this.Controls.Add(this.buttonAsinkroni);
             this.Controls.Add(this.buttonSinkroni);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Forma";
             this.Text = "Moj Marljivi Program";
