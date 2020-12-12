02aa78f587d787ca40bb41e9496cae7b15b5a17a
Kresimir Bratkovic <kbratkovic@gmail.com>
2020-12-12 21:49:09 +0100

    Resolve out parameter

--- OutParametar/OutParametar.cs
+++ OutParametar/OutParametar.cs
@@ -4,8 +4,8 @@ namespace Vsite.CSharp.Metode
 {
     class OutParametar
     {
-        // TODO:050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
-        static void InicijalizirajBrojNaDeset(int x)
+        //  Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
+        static void InicijalizirajBrojNaDeset(out int x)
         {
             x = 10;
         }
@@ -13,7 +13,7 @@ namespace Vsite.CSharp.Metode
         public static void PozivInicijalizacijeOutParametrom()
         {
             int x = 0;
-            InicijalizirajBrojNaDeset(x);
+            InicijalizirajBrojNaDeset(out x);
             Console.WriteLine($"Nakon InicijalizirajBrojNaDeset: {x}");
         }
 
@@ -22,16 +22,17 @@ namespace Vsite.CSharp.Metode
             while (true)
             {
                 Console.WriteLine("Upiši neki cijeli broj:");
-                // TODO:051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
-
-                return 0;
+                string tekst = Console.ReadLine();
+                //  Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
+                if (int.TryParse(tekst, out int rezultat))
+                    return rezultat;
             }
         }
 
-        // TODO:052 Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
+        //  Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
         static void Main(string[] args)
         {
-
+            
             PozivInicijalizacijeOutParametrom();
 
             UčitavanjeCijelogBroja();
