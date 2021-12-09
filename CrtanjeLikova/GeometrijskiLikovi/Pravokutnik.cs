cf5f26dac9b1e61f5789ad4515f549c94176f5fc
Branko Rados <BRados@dspace.hr>
2021-12-09 21:10:59 +0100

    moving elipse into separate file

--- CrtanjeLikova/GeometrijskiLikovi/Pravokutnik.cs
+++ CrtanjeLikova/GeometrijskiLikovi/Pravokutnik.cs
@@ -44,47 +44,4 @@ namespace Vsite.CSharp.Metode.GeometrijskiLikovi
         private Pen border;
         private Brush fill;
     }
-
-    internal class Elipsa : GeometrijskiLik
-    {
-
-        private float x;
-        private float y;
-        private float width;
-        private float height;
-        private Pen border;
-        private Brush fill;
-
-        public Elipsa(float x, float y, float width, float height, Pen border, Brush fill)
-        {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.border = border;
-            this.fill = fill;
-        }
-
-        public override void Pomakni(float deltaX, float deltaY)
-        {
-            x += deltaX;
-            y += deltaY;
-        }
-
-        public override void Uvećaj(float faktor)
-        {
-            this.width *= faktor;
-            this.height *= faktor;
-        }
-
-        protected override void NacrtajOkvir(Graphics g)
-        {
-            g.DrawEllipse(border, x, y, width, height);
-        }
-
-        protected override void Popuni(Graphics g)
-        {
-            g.FillEllipse(fill, x, y, width, height);
-        }
-    }
 }
