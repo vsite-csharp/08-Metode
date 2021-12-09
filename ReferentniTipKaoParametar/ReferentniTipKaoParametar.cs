7a86ddc1329924476986d8ec22fba6752e42703a
Jakov Zorić <jazoric@vsite.hr>
2021-12-09 20:12:20 +0100

    Resolve references

--- ReferentniTipKaoParametar/ReferentniTipKaoParametar.cs
+++ ReferentniTipKaoParametar/ReferentniTipKaoParametar.cs
@@ -34,7 +34,7 @@ namespace Vsite.CSharp.Metode
 
             Console.WriteLine();
 
-            // TODO:032 Dodati poziv metode PromijeniIme s prosljeđenom osobom janko i novim imenom "Pero" i ispisati osobu nakon poziva te metode.
+            // :032 Dodati poziv metode PromijeniIme s prosljeđenom osobom janko i novim imenom "Pero" i ispisati osobu nakon poziva te metode.
             PromijeniIme(janko, "Pero");
             Console.WriteLine(janko);
             // :033 Pokrenuti program i obrazložiti ispis.
