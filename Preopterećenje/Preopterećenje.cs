bf8b469da0ad4c163ea22010b3c5c77d90ad63c9
Ana Lovric <alovric@dspace.hr>
2021-12-09 19:38:11 +0100

    00: Resolved method overloading.

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -4,17 +4,22 @@ namespace Vsite.CSharp.Metode
 {
     class Preopterećenje
     {
-        // TODO:000 Pokrenuti program i provjeriti ispis.
+        // 000 Pokrenuti program i provjeriti ispis.
         public static void MojaMetoda(double broj1, double broj2)
         {
             Console.WriteLine($"MojaMetoda(double {broj1}, double {broj2})");
         }
 
-        // TODO:001 Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+        // 001 Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int,
+        // a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+        public static void MojaMetoda(int broj1, double broj2)
+        {
+            Console.WriteLine($"MojaMetoda(int {broj1}, double {broj2})");
+        }
 
-        // TODO:002 Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
+        // 002 Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
 
-        // TODO:003 Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
+        // 003 Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
 
 
         static void Main(string[] args)
@@ -24,10 +29,10 @@ namespace Vsite.CSharp.Metode
             double d1 = 3;
             double d2 = 4;
 
-            MojaMetoda(i1, i2);
+            MojaMetoda(i1, i2); //zove se druga metoda, jer se trazi sto manje implicintnih pretvorbi!
             MojaMetoda(i1, d1);
-            MojaMetoda(d1, d2);
-            MojaMetoda(d1, i2);
+            MojaMetoda(d1, d2);//prva
+            MojaMetoda(d1, i2);//prva jer double ne moze u int!
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
