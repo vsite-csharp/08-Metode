15fd09cb41e44d88e53da0dc4002f8b59b3ee77c
jandela <andjela.cirjak@gmail.com>
2021-12-09 19:37:57 +0100

    Resolve method overloading.

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -4,17 +4,20 @@ namespace Vsite.CSharp.Metode
 {
     class Preopterećenje
     {
-        // TODO:000 Pokrenuti program i provjeriti ispis.
+        // Pokrenuti program i provjeriti ispis.
         public static void MojaMetoda(double broj1, double broj2)
         {
             Console.WriteLine($"MojaMetoda(double {broj1}, double {broj2})");
         }
 
-        // TODO:001 Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+        // Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+        public static void MojaMetoda(int broj1, double broj2)
+        {
+            Console.WriteLine($"MojaMetoda(int {broj1}, double {broj2})");
+        }
+        // Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
 
-        // TODO:002 Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
-
-        // TODO:003 Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
+        // Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
 
 
         static void Main(string[] args)
@@ -24,10 +27,10 @@ namespace Vsite.CSharp.Metode
             double d1 = 3;
             double d2 = 4;
 
-            MojaMetoda(i1, i2);
-            MojaMetoda(i1, d1);
-            MojaMetoda(d1, d2);
-            MojaMetoda(d1, i2);
+            MojaMetoda(i1, i2); //int, double
+            MojaMetoda(i1, d1); //int, double
+            MojaMetoda(d1, d2); //double, double
+            MojaMetoda(d1, i2); //double, double
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
