4936fc842dc3f68d6f91eda12c421e92e36395cb
Andrea Vrljic <andrea31542@gmail.com>
2021-12-09 19:46:38 +0100

    Resolve method default arguments

--- PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
+++ PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
@@ -4,26 +4,33 @@ namespace Vsite.CSharp.Metode
 {
     class PreopterećenjePodrazumijevaniArg
     {
-        // TODO:010 Razmotriti što će se pokretanjem programa ispisati. Pokrenuti program i provjeriti ispise.
+        // :010 Razmotriti što će se pokretanjem programa ispisati. Pokrenuti program i provjeriti ispise.
         public static int Zbroji(int x = 1, int y = 2, int z = 3)
         {
             return x + y + z;
         }
 
-        // TODO:011 Preopteretiti funkciju Zbroji novom funkcijom public static int Zbroji(int x, int y) koja će vratiti zbroj argumenata. 
+        // :011 Preopteretiti funkciju Zbroji novom funkcijom public static int Zbroji(int x, int y) koja će vratiti zbroj argumenata. 
+        public static int Zbroji(int x, int y )
+        {
+            return x + y; 
+        }
+        // Nakon stvaranja ove metode u drugom slučaju se poziva ova metoda
+        // Mogu se definirati sa argumentiam od kraja metode
 
-        // TODO:012 Razmotriti koje će se inačice metoda pozvati. Ponovno pokrenuti programa te usporediti ispise.
+        // :012 Razmotriti koje će se inačice metoda pozvati. Ponovno pokrenuti programa te usporediti ispise.
 
-        // TODO:013 Provjeriti što bi se dogodilo ako bismo drugoj metodi definirali y s podrazumijevanim argumentom.
+        // :013 Provjeriti što bi se dogodilo ako bismo drugoj metodi definirali y s podrazumijevanim argumentom.
+        // Treći slučaj se ne može izvršiti 
 
-        // TODO:014 Pokrenuti i provjeriti testove (3 testa "TestPreopterećenjePodrazumijevaniArg" moraju proći).
+        // :014 Pokrenuti i provjeriti testove (3 testa "TestPreopterećenjePodrazumijevaniArg" moraju proći).
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Zbroji(5, 6, 7));
-            Console.WriteLine(Zbroji(5, 5));
-            Console.WriteLine(Zbroji(3));
+            Console.WriteLine(Zbroji(5, 6, 7)); //18
+            Console.WriteLine(Zbroji(5, 5)); //13
+            Console.WriteLine(Zbroji(3)); // 8
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
