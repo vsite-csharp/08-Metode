a71990f57cdad38284e4766dba1d11fa86fe38c4
robilovric <robilovric7@gmail.com>
2021-12-09 20:29:02 +0100

    Resolve out parameters

--- OutParametar/OutParametar.cs
+++ OutParametar/OutParametar.cs
@@ -4,16 +4,16 @@ namespace Vsite.CSharp.Metode
 {
     class OutParametar
     {
-        // TODO:050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
-        static void InicijalizirajBrojNaDeset(int x)
+        //:050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
+        static void InicijalizirajBrojNaDeset(out int x) //kod ref inicijaliziran objekt prije poziva ovdi ne
         {
             x = 10;
         }
 
         public static void PozivInicijalizacijeOutParametrom()
         {
-            int x = 0;
-            InicijalizirajBrojNaDeset(x);
+            //int x = 0;
+            InicijalizirajBrojNaDeset(out int x);
             Console.WriteLine($"Nakon InicijalizirajBrojNaDeset: {x}");
         }
 
@@ -22,13 +22,14 @@ namespace Vsite.CSharp.Metode
             while (true)
             {
                 Console.WriteLine("Upiši neki cijeli broj:");
-                // TODO:051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
-
-                return 0;
+                //:051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
+                var text = Console.ReadLine();
+                if (int.TryParse(text, out int rezultat))
+                    return rezultat;
             }
         }
 
-        // TODO:052 Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
+        //:052 Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
         static void Main(string[] args)
         {
 
