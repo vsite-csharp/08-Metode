27156ea50ddf0b24dd0e24a85e69a010d0080662
Andrea Vrljic <andrea31542@gmail.com>
2021-12-09 19:55:01 +0100

    Resolve value types as arguments

--- VrijednosniTipKaoParametar/VrijednosniTipKaoParametar.cs
+++ VrijednosniTipKaoParametar/VrijednosniTipKaoParametar.cs
@@ -41,20 +41,25 @@ namespace Vsite.CSharp.Metode
             Osoba janko = new Osoba("Janko", 1);
             Console.WriteLine(janko);
 
-            // TODO:021 Dodati poziv metode ZamijeniOsobuDarkom s prosljeđenom osobom janko i ispisati osobu nakon poziva te metode.
+            // :021 Dodati poziv metode ZamijeniOsobuDarkom s prosljeđenom osobom janko i ispisati osobu nakon poziva te metode.
+            ZamijeniOsobuDarkom(janko);
+            Console.WriteLine(janko);
+            //Metoda radi sa kopijom objekta pa je janko i dalje janko
 
-            // TODO:022 Pokrenuti program i obrazložiti ispis.
+            // :022 Pokrenuti program i obrazložiti ispis.
 
             Console.WriteLine();
 
-            // TODO:023 Dodati poziv metode PromijeniIme s prosljeđenom osobom janko i novim imenom "Pero" i ispisati osobu nakon poziva te metode.
+            // :023 Dodati poziv metode PromijeniIme s prosljeđenom osobom janko i novim imenom "Pero" i ispisati osobu nakon poziva te metode.
+            PromijeniIme(janko, "Pero");
+            Console.WriteLine(janko); //Janko i dalje, metoda rukuje sa kopijom objekta
 
-            // TODO:024 Pokrenuti program i obrazložiti ispis.
+            // :024 Pokrenuti program i obrazložiti ispis.
 
-            // TODO:025 Pokrenuti i provjeriti testove (test TestPromjeneArgumentaVrijednosnogTipa treba proći)
+            // :025 Pokrenuti i provjeriti testove (test TestPromjeneArgumentaVrijednosnogTipa treba proći)
         }
 
-        // TODO:020 Pokrenuti program, provjeriti ispise te ih obrazložiti.
+        // :020 Pokrenuti program, provjeriti ispise te ih obrazložiti.
         static void Main(string[] args)
         {
             UvećavanjeVrijednosnogParametra();
