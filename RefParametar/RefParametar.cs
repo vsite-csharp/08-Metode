a598aaa068f3e1e8e502906478084495e70eff04
robilovric <robilovric7@gmail.com>
2021-12-09 20:15:47 +0100

    Resolve pass by ref

--- RefParametar/RefParametar.cs
+++ RefParametar/RefParametar.cs
@@ -6,8 +6,9 @@ namespace Vsite.CSharp.Metode
 
     class RefParametar
     {
-        // TODO:040 Dodati parametru metode modifikator ref tako da se argument x metodi prenosi po referenci te promijeniti poziv metode. Pokrenuti program i provjeriti ispis.
-        static void DodajDeset(int x)
+        //:040 Dodati parametru metode modifikator ref tako da se argument x metodi prenosi po referenci te promijeniti poziv metode. Pokrenuti program i provjeriti ispis.
+
+        static void DodajDeset(ref int x)
         {
             x += 10;
         }
@@ -15,13 +16,13 @@ namespace Vsite.CSharp.Metode
         public static int PozivMetodeDodajDeset(int broj)
         {
             Console.WriteLine($"Prije metode DodajDeset: {broj}");
-            DodajDeset(broj);
+            DodajDeset(ref broj);
             Console.WriteLine($"Nakon metode DodajDeset: {broj}");
             return broj;
         }
 
-        // TODO:041 Dodati prvom parametru metode modifikator ref tako da se prvi argument osoba metodi prenosi po referenci te promijeniti poziv metode. Pokrenuti program i provjeriti ispis.
-        static void PromijeniOsobu(Osoba osoba, string novoIme, int noviMatičniBroj)
+        //:041 Dodati prvom parametru metode modifikator ref tako da se prvi argument osoba metodi prenosi po referenci te promijeniti poziv metode. Pokrenuti program i provjeriti ispis.
+        static void PromijeniOsobu(ref Osoba osoba, string novoIme, int noviMatičniBroj)
         {
             osoba = new Osoba(novoIme, noviMatičniBroj);
         }
@@ -29,12 +30,12 @@ namespace Vsite.CSharp.Metode
         public static Osoba PozivMetodePromijeniOsobu(Osoba osoba, string novoIme, int noviMatičniBroj)
         {
             Console.WriteLine($"Prije metode PromijeniOsobu: {osoba}");
-            PromijeniOsobu(osoba, novoIme, noviMatičniBroj);
+            PromijeniOsobu(ref osoba, novoIme, noviMatičniBroj);
             Console.WriteLine($"Nakon metode PromijeniOsobu: {osoba}");
             return osoba;
         }
 
-        // TODO:042 Pokrenuti i provjeriti testove (2 testa TestRefParametar moraju proći).
+        //:042 Pokrenuti i provjeriti testove (2 testa TestRefParametar moraju proći).
         static void Main(string[] args)
         {
             PozivMetodeDodajDeset(5);
