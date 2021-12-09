b3d1358ffad846b2663d9e08b350dcaf381c9328
Andrea Vrljic <andrea31542@gmail.com>
2021-12-09 20:26:43 +0100

    Resolve OutParameter

--- OutParametar/OutParametar.cs
+++ OutParametar/OutParametar.cs
@@ -4,16 +4,18 @@ namespace Vsite.CSharp.Metode
 {
     class OutParametar
     {
-        // TODO:050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
-        static void InicijalizirajBrojNaDeset(int x)
+        // :050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
+        static void InicijalizirajBrojNaDeset(out int x)
         {
             x = 10;
         }
+        // kod refa- metodi prosljeđujemo referencu i direktno pristupamo objektu, a ponekad želimo da nam vrati podatak koji nije inicijaliziran prije poziva metode
+        //ref kompajler očekuje inicijaizaciju objekta prije poziva metode, a kod out ne
 
         public static void PozivInicijalizacijeOutParametrom()
         {
-            int x = 0;
-            InicijalizirajBrojNaDeset(x);
+            //int x = 0; Nije nam potrebna inicijalizacije, al dole mormao deklarirati tip objekta
+            InicijalizirajBrojNaDeset(out int x);
             Console.WriteLine($"Nakon InicijalizirajBrojNaDeset: {x}");
         }
 
@@ -22,13 +24,20 @@ namespace Vsite.CSharp.Metode
             while (true)
             {
                 Console.WriteLine("Upiši neki cijeli broj:");
-                // TODO:051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
+                // :051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
+                var tekst = Console.ReadLine();
+                if (int.TryParse(tekst, out int rezultat))
+                {
+                    return rezultat;
+                };
 
-                return 0;
+                // parse parsira tekst, ako ne uspije baci exception
+                //tryparse kada ne želimo da nam baci exception, ako ne može prevesti vraća bool je li uspjela ili nije uspjela, 
+                //ako je uspjela vrati kroz drugi parametar rezultat
             }
         }
 
-        // TODO:052 Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
+        // :052 Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
         static void Main(string[] args)
         {
 
