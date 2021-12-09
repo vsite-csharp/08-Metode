0cccf577dc99db21e7523a4ad4eff7c2070dc3b7
Robert Tušek <kratos365@gmail.hr>
2021-12-09 21:24:43 +0100

    Resolve

--- CrtanjeLikova/GeometrijskiLikovi/Crtež.cs
+++ CrtanjeLikova/GeometrijskiLikovi/Crtež.cs
@@ -42,11 +42,11 @@ namespace Vsite.CSharp.Metode.GeometrijskiLikovi
 
             // TODO:091 U projekt dodati novu klasu Elipsa, izvedenu iz klase GeometrijskiLik te implemenirati potrebne metode.
 
-            // TODO:092 Otkomentirati donje naredbe i pokrenuti program.
-            //DodajLik(new Elipsa(80, 60, 60, 40));
-            //DodajLik(new Elipsa(180, 80, 50, 80));
+            // :092 Otkomentirati donje naredbe i pokrenuti program.
+            DodajLik(new Elipsa(80, 60, 60, 40));
+            DodajLik(new Elipsa(180, 80, 50, 80));
 
-            // TODO:097 Dodati u gornjim konstruktorima boju popune i boju okvira za svaki lik te pokrenuti program.
+            // :097 Dodati u gornjim konstruktorima boju popune i boju okvira za svaki lik te pokrenuti program.
         }
 
         private List<GeometrijskiLik> likovi = new List<GeometrijskiLik>();
