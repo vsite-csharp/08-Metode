b0e2b369c18f3627b6dda4c7c44e58ef1d599812
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2021-12-09 21:25:45 +0100

    Resolve painting

--- CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -1,10 +1,15 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Vsite.CSharp.Metode.GeometrijskiLikovi
 {
-    class Pravokutnik : GeometrijskiLik
+    class Elipsa : GeometrijskiLik
     {
-        public Pravokutnik(float x, float y, float širina, float visina)
+        public Elipsa(float x, float y, float širina, float visina)
         {
             this.x = x;
             this.y = y;
@@ -12,33 +17,30 @@ namespace Vsite.CSharp.Metode.GeometrijskiLikovi
             this.visina = visina;
         }
 
+
         protected override void NacrtajOkvir(Graphics g)
         {
-            g.DrawRectangle(Pens.Black, x, y, širina, visina);
+            g.DrawEllipse(Pens.Violet, x, y, širina, visina);
         }
 
         public override void Pomakni(float deltaX, float deltaY)
         {
-            x += deltaX;
-            y += deltaY;
+            throw new NotImplementedException();
         }
 
         public override void Uvećaj(float faktor)
         {
-            širina *= faktor;
-            visina *= faktor;
+            
         }
 
         protected override void Popuni(Graphics g)
         {
-            g.FillRectangle(new SolidBrush(Color.Red), x, y, širina, visina);
+            g.FillRectangle(new SolidBrush(Color.Aquamarine), x, y, širina, visina);
+
         }
 
-        public override void NacrtajOkvir(Graphics g)
-        {
-            throw new System.NotImplementedException();
-        }
-
+       
+      
         private float x;
         private float y;
         private float širina;
