afada51abc209032153a06c1eb341f1058b50236
Tomislav Petrović <topetrov@vsite.hr>
2021-12-09 20:53:17 +0100

    resolve generic swaping

--- ZamjenaObjekata/ZamjenaObjekata.cs
+++ ZamjenaObjekata/ZamjenaObjekata.cs
@@ -4,8 +4,15 @@ namespace Vsite.CSharp.Metode
 {
     class ZamjenaObjekata
     {
-        // TODO:061 Promijeniti metodu tako da se zamjena odrazi u pozivajućem kodu.
-        static void Zamijeni(string s1, string s2)
+        //061 Promijeniti metodu tako da se zamjena odrazi u pozivajućem kodu.
+        static void Zamijeni(ref string s1,ref string s2)
+        {
+            string temp = s2;
+            s2 = s1;
+            s1 = temp;
+        }
+
+        static void Zamijeni(ref object s1, ref object s2)
         {
             string temp = s2;
             s2 = s1;
@@ -18,7 +25,7 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
 
-            Zamijeni(prvi, drugi);
+            Zamijeni(ref prvi,ref drugi);
 
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
@@ -32,7 +39,10 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("drugi = '{0}'", drugi);
 
             // TODO:062 Preopteretiti metodu Zamijeni tako da umjesto tipa string prima tip object i tako podržava zamjenu bilo kojeg tipa objekta. Dodati poziv te metode za 2 argumenta tipa int te provjeriti funkcionalnost.
-
+            
+            object obj1 = prvi;
+            object o2 = drugi;
+            Zamijeni(ref prvi,ref drugi);
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
