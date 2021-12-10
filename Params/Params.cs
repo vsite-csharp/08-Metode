8cb806513ef2de10ace056813e28aa42c2a01f74
jandela <andjela.cirjak@gmail.com>
2021-12-10 13:24:59 +0100

    Resolve multiple params method

--- Params/Params.cs
+++ Params/Params.cs
@@ -5,20 +5,23 @@ namespace Vsite.CSharp.Metode
 
     class Params
     {
-        // TODO:110 Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
+        // Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
         public static double Zbroji(params double[] pribrojnici)
         {
-            return 0.0;
+            var suma = 0.0;
+            foreach (var el in pribrojnici)
+                suma += el;
+            return suma;
         }
 
-        // TODO:111 Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
+        // Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
 
         static void Main(string[] args)
         {
-            double zbroj = Zbroji(1, 2, 3, 4);
+            double zbroj = Zbroji(1, 2, 3, 4); //10
             Console.WriteLine(zbroj);
 
-            Console.WriteLine(Zbroji(3.2, 4.3, 7.5));
+            Console.WriteLine(Zbroji(3.2, 4.3, 7.5)); //15
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
