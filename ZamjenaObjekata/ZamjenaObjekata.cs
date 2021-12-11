9f7b5d6b9c4e9f7596dfa982c4e6d315a1fc919f
Matko Stojić <mastojic@vsite.hr>
2021-12-11 14:58:19 +0100

    prepravke 2

--- ZamjenaObjekata/ZamjenaObjekata.cs
+++ ZamjenaObjekata/ZamjenaObjekata.cs
@@ -23,11 +23,9 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("Prije metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
-            object o1 = prvi;
-            object o2 = drugi;
-            Zamijeni(ref o1,ref o2);
-            prvi = string(o1);
-            drugi = string(o2);
+            
+            Zamijeni(ref prvi,ref drugi);
+           
 
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
@@ -41,11 +39,11 @@ namespace Vsite.CSharp.Metode
             Console.WriteLine("drugi = '{0}'", drugi);
 
             // 062 Preopteretiti metodu Zamijeni tako da umjesto tipa string prima tip object i tako podržava zamjenu bilo kojeg tipa objekta. Dodati poziv te metode za 2 argumenta tipa int te provjeriti funkcionalnost.
-            object o1 = prvi;
-            object o2 = drugi;
-            Zamijeni(ref o1, ref o2);
-            prvi = int(o1);
-            drugi = int(o2);
+            var p1 = (object)prvi;
+            var p2 = (object)drugi;
+            Zamijeni(ref p1, ref p2);
+            prvi = (int)p1;
+            drugi = (int)p2;
 
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
