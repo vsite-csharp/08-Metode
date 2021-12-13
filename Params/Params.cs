524f508cd9d593ddc72e597be389ba28eaeaf0dd
Pavle Pavicic <ppavicic@dspace.hr>
2021-12-13 16:23:17 +0100

    Solved all TODO:11*.

--- Params/Params.cs
+++ Params/Params.cs
@@ -5,13 +5,19 @@ namespace Vsite.CSharp.Metode
 
     class Params
     {
-        // TODO:110 Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
+        // Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
         public static double Zbroji(params double[] pribrojnici)
         {
-            return 0.0;
+            double suma = 0.0;
+            foreach (double pribrojnik in pribrojnici)
+            {
+                suma += pribrojnik;
+            }
+
+            return suma;
         }
 
-        // TODO:111 Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
+        // Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
 
         static void Main(string[] args)
         {
