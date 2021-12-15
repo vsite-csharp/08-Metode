21afb693561b20a77a3d6df41f9a50dfb0e35b76
Mihael1324 <mihael.vlainic@vsite.hr>
2021-12-15 18:38:44 +0100

    ispravak

--- CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -1,46 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Vsite.CSharp.Metode.GeometrijskiLikovi
 {
-    
     class Elipsa : GeometrijskiLik
     {
-        public Elipsa(float x, float y, float širina, float visina)
+
+        private float x;
+        private float y;
+        private float width;
+        private float height;
+        private Pen border;
+        private Brush fill;
+
+        public Elipsa(float x, float y, float width, float height, Pen border, Brush fill)
         {
             this.x = x;
             this.y = y;
-            this.širina = širina;
-            this.visina = visina;
-        }
-        public override void Nacrtaj(Graphics g)
-        {
-            
-            g.DrawEllipse(Pens.Violet, x, y, širina, visina);
+            this.width = width;
+            this.height = height;
+            this.border = border;
+            this.fill = fill;
         }
 
         public override void Pomakni(float deltaX, float deltaY)
         {
-            //g.FillElipse(new SolidBrush(Color.Aquamarine), x, y, širina, visina);
-        }
-
-        public override void Popuni(Graphics g)
-        {
-            g.FillElipse(new SolidBrush(Color.Aquamarine), x, y, širina, visina);
+            x += deltaX;
+            y += deltaY;
         }
 
         public override void Uvećaj(float faktor)
         {
-            throw new NotImplementedException();
+            this.width *= faktor;
+            this.height *= faktor;
         }
 
-        private float x;
-        private float y;
-        private float širina;
-        private float visina;
+        protected override void NacrtajOkvir(Graphics g)
+        {
+            g.DrawEllipse(border, x, y, width, height);
+        }
+
+        protected override void Popuni(Graphics g)
+        {
+            g.FillEllipse(fill, x, y, width, height);
+        }
     }
 }
