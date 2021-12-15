b8de48082ab386186e3c2bdfa2c60515eca260f8
Mihael1324 <mihael.vlainic@vsite.hr>
2021-12-15 18:41:31 +0100

    ispravak

--- Params/Params.cs
+++ Params/Params.cs
@@ -5,13 +5,20 @@ namespace Vsite.CSharp.Metode
 
     class Params
     {
-        // TODO:110 Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
+        // 110 Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
         public static double Zbroji(params double[] pribrojnici)
         {
-            return 0.0;
+            double sum = 0.0d;
+
+            foreach (var pribrojnik in pribrojnici)
+            {
+                sum += pribrojnik;
+            }
+
+            return sum;
         }
 
-        // TODO:111 Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
+        // 111 Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
 
         static void Main(string[] args)
         {
