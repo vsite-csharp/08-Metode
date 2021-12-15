879cca1bce0a8356f9a00dbee1c2561b5fe5fdee
Mihael1324 <mihael.vlainic@vsite.hr>
2021-12-15 18:42:12 +0100

    ispravak

--- MetodeProširenja/MetodeProširenja.cs
+++ MetodeProširenja/MetodeProširenja.cs
@@ -1,28 +1,36 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Vsite.CSharp.Metode
 {
-    // TODO:120 U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
+    //  U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
     public static class MojaProširenja
     {
+        //  Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
+        public static int BrojRiječi(this string express)
+        {
+            var replExpress = Regex.Replace(express, @"[.:;!.,]", " ").Trim();
+            return Regex.Split(replExpress, @"\s+").Length;
+        }
     }
 
+
+    //  Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
     class MetodeProširenja
     {
         public static int DajBrojRiječi(string tekst)
         {
-            // TODO:121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
-            return 0;
+            // Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
+            return tekst.BrojRiječi();
         }
 
-        // TODO:122 Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
-
+        // Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
         static void Main(string[] args)
         {
-            string s = "Kako je počeo rat na mom otoku?";
+            string s = "Ovo je neki tekst koji nema veze sa vezom";
             Console.WriteLine("U '{0}' ima {1} riječi.", s, DajBrojRiječi(s));
 
-            s = "Brojim: jedan, dva, tri! Tko se nije skrio...";
+            s = "Ovaj tekst ce prebrojati sve rijeci";
             Console.WriteLine("U '{0}' ima {1} riječi.", s, DajBrojRiječi(s));
 
             Console.WriteLine("GOTOVO!!!");
