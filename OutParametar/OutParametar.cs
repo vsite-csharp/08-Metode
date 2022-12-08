c658ddd543ff128f2380c2a51193ee855d34b28f
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2022-12-08 21:39:05 +0100

    Resolve out parameter

--- OutParametar/OutParametar.cs
+++ OutParametar/OutParametar.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using System.Runtime.Versioning;
 
 namespace Vsite.CSharp.Metode
 {
     class OutParametar
     {
-        // TODO:050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
-        static void InicijalizirajBrojNaDeset(int x)
+        // :050 Dodati parametru metode modifikator out te promijeniti metodu PozivInicijalizacijeOutParametrom tako da se može prevesti i izvesti.
+        static void InicijalizirajBrojNaDeset(out int x)
         {
             x = 10;
         }
 
         public static void PozivInicijalizacijeOutParametrom()
         {
-            int x = 0;
-            InicijalizirajBrojNaDeset(x);
+            //int x = 0;
+            InicijalizirajBrojNaDeset(out int x);
             Console.WriteLine($"Nakon InicijalizirajBrojNaDeset: {x}");
         }
 
@@ -22,13 +23,17 @@ namespace Vsite.CSharp.Metode
             while (true)
             {
                 Console.WriteLine("Upiši neki cijeli broj:");
-                // TODO:051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
-
-                return 0;
+                var unos = Console.ReadLine();
+                // :051 Dodati poziv metode int.TryParse koja će upisani znakovni niz pretvoriti u cijeli broj i vratiti to kao rezultat metode UčitavanjeCijelogBroja.
+                if(int.TryParse(unos, out int rezultat))
+                {
+                    return rezultat;
+                }
+                
             }
         }
 
-        // TODO:052 Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
+        // :052 Pokrenuti i provjeriti testove (oba testa u grupi TestOutParametar moraju proći).
         static void Main(string[] args)
         {
             PozivInicijalizacijeOutParametrom();
