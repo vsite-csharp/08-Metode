5d2bd0b10b22708c5a6cc78ff332741d4dba492e
Giancarlo <giancarlo.bedek@hotmail.com>
2022-12-15 20:34:30 +0100

    Resolve params

--- Params/Params.cs
+++ Params/Params.cs
@@ -4,13 +4,13 @@ namespace Vsite.CSharp.Metode
 {
     class Params
     {
-        // TODO:110 Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
+        // Napisati implementaciju metode tako da vrati zbroj svih proslijeđenih argumenata. Pokrenuti program i provjeriti rezultate.
         public static double Zbroji(params double[] pribrojnici)
         {
-            return 0.0;
+            return pribrojnici.Sum();
         }
 
-        // TODO:111 Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
+        // Pokrenuti i provjeriti testove (3 testa u grupi TestParams moraju proći).
 
         static void Main(string[] args)
         {
