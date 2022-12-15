60014fbf041e12eaac8df43922db655d66cc7ae8
ipandzicd <ivan.pandzic@gmail.com>
2022-12-15 20:19:20 +0100

    Partially resolve shape drawing

---
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Vsite.CSharp.Metode.GeometrijskiLikovi
+{
+    internal class Elipsa : GeometrijskiLik
+    {
+        public Elipsa(float x, float y, float širina, float visina)
+        {
+            this.x = x;
+            this.y = y;
+            this.širina = širina;
+            this.visina = visina;
+        }
+
+        private float x;
+        private float y;
+        private float širina;
+        private float visina;
+
+        public override void Nacrtaj(Graphics g)
+        {
+            g.DrawEllipse(Pens.Aquamarine);
+            throw new NotImplementedException();
+        }
+
+        public override void Pomakni(float deltaX, float deltaY)
+        {
+            x += deltaX;
+            y += deltaY;
+        }
+
+        public override void Uvećaj(float faktor)
+        {
+            širina *= faktor;
+            visina *= faktor;
+        }
+    }
+}
