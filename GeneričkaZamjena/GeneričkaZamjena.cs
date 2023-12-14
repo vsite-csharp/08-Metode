a816eab937a2cb5859118d746bdd620b54e44144
Ivan Davidović <hedead421@gmail.com>
2023-12-14 21:26:07 +0100

    Test virtual and nonvirtual methodes

--- GeneričkaZamjena/GeneričkaZamjena.cs
+++ GeneričkaZamjena/GeneričkaZamjena.cs
@@ -2,7 +2,7 @@
 {
     static class GeneričkaZamjena
     {
-        // TODO:070 Definirati javno dostupnu statičku generičku metodu Zamijeni<T>, koja će biti parametrizirana tipom T i moći raditi tipski sigurnu zamjenu bilo koja dva objekta istog tipa.
+        // :070 Definirati javno dostupnu statičku generičku metodu Zamijeni<T>, koja će biti parametrizirana tipom T i moći raditi tipski sigurnu zamjenu bilo koja dva objekta istog tipa.
         public static void Zamijeni<T>(ref T Prvi, ref T Drugi)
         {
             var temp = Drugi;
@@ -20,7 +20,7 @@
             int i1 = 5;
             int i2 = 10;
             IspišiPar(i1, i2);
-            // TODO:071 Dodati poziv metode Zamijeni<T> za i1 i i2, pokrenuti program i provjeriti ispis.
+            // :071 Dodati poziv metode Zamijeni<T> za i1 i i2, pokrenuti program i provjeriti ispis.
             Zamijeni<int>(ref i1, ref i2);
             IspišiPar(i1, i2);
 
@@ -29,7 +29,7 @@
             double d1 = 1.23;
             double d2 = 4.56;
             IspišiPar(d1, d2);
-            // TODO:072 Dodati poziv metode Zamijeni<T> za d1 i d2, pokrenuti program i provjeriti ispis.
+            // :072 Dodati poziv metode Zamijeni<T> za d1 i d2, pokrenuti program i provjeriti ispis.
             Zamijeni<double>(ref d1, ref d2);
             IspišiPar(d1, d2);
 
@@ -38,13 +38,13 @@
             string s1 = "Ivica";
             string s2 = "Marica";
             IspišiPar(s1, s2);
-            // TODO:073 Dodati poziv metode Zamijeni<T> za s1 i s2, pokrenuti program i provjeriti ispis.
+            // :073 Dodati poziv metode Zamijeni<T> za s1 i s2, pokrenuti program i provjeriti ispis.
             Zamijeni<string>(ref s1, ref s2);
             IspišiPar(s1, s2);
 
-            // TODO:074 Pokrenuti i provjeriti testove (3 testa u grupi TestGeneričkeZamjene moraju proći).
+            // :074 Pokrenuti i provjeriti testove (3 testa u grupi TestGeneričkeZamjene moraju proći).
 
-            // TODO:075 Pogledati generirani kod metode Zamijeni alatom ILDASM.
+            // :075 Pogledati generirani kod metode Zamijeni alatom ILDASM.
 
         }
     }
