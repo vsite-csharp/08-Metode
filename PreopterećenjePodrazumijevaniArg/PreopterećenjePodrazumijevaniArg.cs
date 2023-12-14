03a1de7a019d0d119406f4705ed58a28f48320b4
Bogjenero <bornagjenero1@gmail.com>
2023-12-14 20:14:53 +0100

    Resolve overloading methods with default parameters

--- PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
+++ PreopterećenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
@@ -2,19 +2,22 @@
 {
     static class PreopterećenjePodrazumijevaniArg
     {
-        // TODO:010 Razmotriti što će se pokretanjem programa ispisati. Pokrenuti program i provjeriti ispise.
+        // :010 Razmotriti što će se pokretanjem programa ispisati. Pokrenuti program i provjeriti ispise.
         public static int Zbroji(int x, int y = 2, int z = 3)
         {
             return x + y + z;
         }
+        // :011 Preopteretiti funkciju Zbroji novom funkcijom public static int Zbroji(int x, int y) koja će vratiti zbroj argumenata. 
+           public static int Zbroji(int x, int y)
+        {
+            return x + y;
+        }
 
-        // TODO:011 Preopteretiti funkciju Zbroji novom funkcijom public static int Zbroji(int x, int y) koja će vratiti zbroj argumenata. 
+        // :012 Razmotriti koje će se inačice metoda pozvati. Ponovno pokrenuti programa te usporediti ispise.
 
-        // TODO:012 Razmotriti koje će se inačice metoda pozvati. Ponovno pokrenuti programa te usporediti ispise.
+        // :013 Provjeriti što bi se dogodilo ako bismo drugoj metodi definirali y s podrazumijevanim argumentom.
 
-        // TODO:013 Provjeriti što bi se dogodilo ako bismo drugoj metodi definirali y s podrazumijevanim argumentom.
-
-        // TODO:014 Pokrenuti i provjeriti testove (3 testa "TestPreopterećenjePodrazumijevaniArg" moraju proći).
+        // :014 Pokrenuti i provjeriti testove (3 testa "TestPreopterećenjePodrazumijevaniArg" moraju proći).
 
 
         static void Main()
