54de898e3179b197756e464dfc4f3de2a1ffe8cd
Josip Grabušić <jograbus@vsite.hr>
2023-12-14 21:01:17 +0100

    Resolve object swapping

--- ZamjenaObjekata/ZamjenaObjekata.cs
+++ ZamjenaObjekata/ZamjenaObjekata.cs
@@ -2,8 +2,8 @@
 {
     static class ZamjenaObjekata
     {
-        // TODO:061 Promijeniti metodu tako da se zamjena odrazi u pozivajućem kodu.
-        static void Zamijeni(string s1, string s2)
+        // :061 Promijeniti metodu tako da se zamjena odrazi u pozivajućem kodu.
+        static void Zamijeni(ref object s1, ref object s2)
         {
             var temp = s2;
             s2 = s1;
@@ -16,7 +16,11 @@
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
 
-            Zamijeni(prvi, drugi);
+            object o1 = prvi;
+            object o2 = drugi;
+            Zamijeni(ref o1, ref o2);
+            prvi = (string)o1;
+            drugi = (string)o2;
 
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
@@ -29,16 +33,21 @@
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
 
-            // TODO:062 Promijeniti metodu Zamijeni tako da umjesto tipa string prima tip object i tako podržava zamjenu bilo kojeg tipa objekta. Dodati poziv te metode za 2 argumenta tipa int te provjeriti funkcionalnost.
+            // :062 Promijeniti metodu Zamijeni tako da umjesto tipa string prima tip object i tako podržava zamjenu bilo kojeg tipa objekta. Dodati poziv te metode za 2 argumenta tipa int te provjeriti funkcionalnost.
+            object o1 = prvi;
+            object o2 = drugi;
+            Zamijeni(ref o1, ref o2);
+            prvi = (int)o1;
+            drugi = (int)o2;
 
             Console.WriteLine("Nakon metode Zamijeni:");
             Console.WriteLine("prvi = '{0}'", prvi);
             Console.WriteLine("drugi = '{0}'", drugi);
         }
 
-        // TODO:063 Pokrenuti i provjeriti testove (oba testa u grupi TestZamjeneObjekata moraju proći)
+        // :063 Pokrenuti i provjeriti testove (oba testa u grupi TestZamjeneObjekata moraju proći)
 
-        // TODO:060 Pokrenuti program i provjeriti ispis.
+        // :060 Pokrenuti program i provjeriti ispis.
         static void Main()
         {
             string prvi = "prvi";
