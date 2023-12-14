c9d2f154e7f73c7f4123d85ca3ce4a6c07d77774
Ivan Fišić <ivfisic@vsite.hr>
2023-12-14 21:26:38 +0100

    Test virtual and non-virtual methods

---
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Metode.GeometrijskiLikovi
+{
+    internal class Elipsa : GeometrijskiLik
+    {
+        public override void Nacrtaj(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Pomakni(float deltaX, float deltaY)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Uvećaj(float faktor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
