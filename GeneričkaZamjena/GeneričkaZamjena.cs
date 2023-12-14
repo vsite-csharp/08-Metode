563a9770fda4a93d8488ea0d32cfe3b682b3a45b
Hrvoje Janđel <hrjandje@vsite.hr>
2023-12-14 21:10:20 +0100

    resolve generic swapping

--- GeneričkaZamjena/GeneričkaZamjena.cs
+++ GeneričkaZamjena/GeneričkaZamjena.cs
@@ -2,8 +2,13 @@
 {
     static class GeneričkaZamjena
     {
-        // TODO:070 Definirati javno dostupnu statičku generičku metodu Zamijeni<T>, koja će biti parametrizirana tipom T i moći raditi tipski sigurnu zamjenu bilo koja dva objekta istog tipa.
-
+        // :070 Definirati javno dostupnu statičku generičku metodu Zamijeni<T>, koja će biti parametrizirana tipom T i moći raditi tipski sigurnu zamjenu bilo koja dva objekta istog tipa.
+        public static void Zamijeni<T> (ref T prvi, ref T drugi)
+        {
+            var temp = drugi;
+            drugi = prvi;
+            prvi = temp;
+        }
 
         private static void IspišiPar(object o1, object o2)
         {
@@ -15,8 +20,8 @@
             int i1 = 5;
             int i2 = 10;
             IspišiPar(i1, i2);
-            // TODO:071 Dodati poziv metode Zamijeni<T> za i1 i i2, pokrenuti program i provjeriti ispis.
-
+            // :071 Dodati poziv metode Zamijeni<T> za i1 i i2, pokrenuti program i provjeriti ispis.
+            Zamijeni<int>(ref i1, ref i2);
             IspišiPar(i1, i2);
 
             Console.WriteLine();
@@ -24,22 +29,23 @@
             double d1 = 1.23;
             double d2 = 4.56;
             IspišiPar(d1, d2);
-            // TODO:072 Dodati poziv metode Zamijeni<T> za d1 i d2, pokrenuti program i provjeriti ispis.
-
+            // DO:072 Dodati poziv metode Zamijeni<T> za d1 i d2, pokrenuti program i provjeriti ispis.
+            Zamijeni(ref d1, ref d2);
             IspišiPar(d1, d2);
+           
 
             Console.WriteLine();
 
             string s1 = "Ivica";
             string s2 = "Marica";
             IspišiPar(s1, s2);
-            // TODO:073 Dodati poziv metode Zamijeni<T> za s1 i s2, pokrenuti program i provjeriti ispis.
-
+            // :073 Dodati poziv metode Zamijeni<T> za s1 i s2, pokrenuti program i provjeriti ispis.
+            Zamijeni(ref s1, ref s2);
             IspišiPar(s1, s2);
+            
+            // :074 Pokrenuti i provjeriti testove (3 testa u grupi TestGeneričkeZamjene moraju proći).
 
-            // TODO:074 Pokrenuti i provjeriti testove (3 testa u grupi TestGeneričkeZamjene moraju proći).
-
-            // TODO:075 Pogledati generirani kod metode Zamijeni alatom ILDASM.
+            // :075 Pogledati generirani kod metode Zamijeni alatom ILDASM.
 
         }
     }
