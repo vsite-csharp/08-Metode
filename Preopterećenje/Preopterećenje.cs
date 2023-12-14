c1bfa18f3218c7ee6ad10aa2af5f9db6abdea89d
Tin Bašić <tin.basic22@gmail.com>
2023-12-14 20:09:30 +0100

    Resolve Preopterećenje

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -2,20 +2,25 @@
 {
     static class Preopterećenje
     {
-        // TODO:000 Pokrenuti program i provjeriti ispis.
+        // :000 Pokrenuti program i provjeriti ispis.
         public static void MojaMetoda(double broj1, double broj2)
         {
             Console.WriteLine($"MojaMetoda(double {broj1}, double {broj2})");
         }
 
-        // TODO:001 Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
+		// :001 Napisati statičku javno dostupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati $"MojaMetoda(int {broj1}, double {broj2})"
 
-        // TODO:002 Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
+		public static void MojaMetoda(int broj1, double broj2)
+		{
+			Console.WriteLine($"MojaMetoda(int {broj1}, double {broj2})");
+		}
 
-        // TODO:003 Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
+		// :002 Razmotriti koje će se inačice pozivati. Pokrenuti program i usporediti ispis s prijašnjim.
+
+		// :003 Pokrenuti i provjeriti testove (4 testa "TestPreopterećenjaMetode" moraju proći).
 
 
-        static void Main()
+		static void Main()
         {
             int i1 = 1;
             int i2 = 2;
