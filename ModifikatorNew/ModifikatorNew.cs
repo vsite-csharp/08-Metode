5587311892b2c2e780879a98aee98e8ae59283b7
Hrvoje Janđel <hrjandje@vsite.hr>
2023-12-18 21:53:17 +0100

    Resolve new modifikator

--- ModifikatorNew/ModifikatorNew.cs
+++ ModifikatorNew/ModifikatorNew.cs
@@ -12,8 +12,13 @@
 
         public class Izvedena : Bazna
         {
-            // TODO:100 Dodati javno dostupnu metodu void PredstaviSe() koja će ispisati "Izvedena klasa, izvedena iz:" te će pozvati PredstaviSe iz klase Bazna.
-
+            // :100 Dodati javno dostupnu metodu void PredstaviSe() koja će ispisati "Izvedena klasa, izvedena iz:" te će pozvati PredstaviSe iz klase Bazna.
+            public new void PredstaviSe()
+            {
+                Console.WriteLine("Izvedena klasa, izvedena iz:");
+                base.PredstaviSe();
+            }
+       
         }
 
         // TODO:101 Izvesti program i usporediti donje ispise.
