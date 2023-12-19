d0610fe40a9b5532eeb5166538baf84414fd5fc3
Tin Bašić <tin.basic22@gmail.com>
2023-12-19 19:12:53 +0100

    Resolve part Crtež - elipsa

---
+++ CrtanjeLikova/GeometrijskiLikovi/Elipsa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.Metode.GeometrijskiLikovi
+{
+    internal class Elipsa:GeometrijskiLik
+    {
+        public Elipsa(float x, float y, float širina, float visina)
+        {
+            this.x = x;
+            this.y = y;
+            this.širina = širina;
+            this.visina = visina;
+        }
+
+        public override void Nacrtaj(Graphics g)
+        {
+            g.DrawRectangle(Pens.Black, x, y, širina, visina);
+        }
+
+        public override void Pomakni(float deltaX, float deltaY)
+        {
+            x += deltaX;
+            y += deltaY;
+        }
+
+        public override void Uvećaj(float faktor)
+        {
+            širina *= faktor;
+            visina *= faktor;
+        }
+
+        private float x;
+        private float y;
+        private float širina;
+        private float visina;
+    }
+}
