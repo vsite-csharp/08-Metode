044705201c54275ba233bc9327651a0c475405e2
Julijan Sribar <julijan.sribar@gmail.com>
2023-12-21 19:38:48 +0100

    Implement extension method for string

--- MetodeProširenja/MetodeProširenja.cs
+++ MetodeProširenja/MetodeProširenja.cs
@@ -1,19 +1,25 @@
 ﻿namespace Vsite.CSharp.Metode
 {
-    // TODO:120 U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
+    // :120 U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
     public static class MojaProširenja
     {
+        public static int BrojRiječi(this string s)
+        {
+            return s.Split(new char[] { ' ', ',', '.', ':', ';', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
     }
 
     static class MetodeProširenja
     {
         public static int DajBrojRiječi(string tekst)
         {
-            // TODO:121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
-            return 0;
+            // :121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
+            return tekst.BrojRiječi();
+
+            //return MojaProširenja.BrojRiječi(tekst);
         }
 
-        // TODO:122 Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
+        // :122 Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
 
         static void Main()
         {
