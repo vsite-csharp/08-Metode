095c8388f5710714c8d5aab4ac3312e977529181
Ivan Davidović <hedead421@gmail.com>
2023-12-21 19:38:55 +0100

    Implement extention methode for string

--- MetodeProširenja/MetodeProširenja.cs
+++ MetodeProširenja/MetodeProširenja.cs
@@ -1,27 +1,31 @@
 ﻿namespace Vsite.CSharp.Metode
 {
-    // TODO:120 U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
+    // :120 U klasu MojaProširenja dodati metodu proširenja (extension method) BrojRiječi klasi string koja kao rezultat vraća broj riječi
     public static class MojaProširenja
     {
+        public static int BrojRiječi(this string s)
+        {
+            return s.Split(new char[] { ' ', ',', '.', ':', ';', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
     }
 
     static class MetodeProširenja
     {
         public static int DajBrojRiječi(string tekst)
         {
-            // TODO:121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
-            return 0;
+            // :121 Zamijeniti povratnu vrijednost rezultatom metode proširenja BrojRiječi. Pokrenuti program i provjeriti ispise.
+            return tekst.BrojRiječi();
         }
 
-        // TODO:122 Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
+            // TODO:122 Pokrenuti i provjeriti testove (4 testa u grupi TestMetodeProširenja moraju proći).
 
-        static void Main()
-        {
-            string s = "Kako je počeo rat na mom otoku?";
-            Console.WriteLine("U '{0}' ima {1} riječi.", s, DajBrojRiječi(s));
+            static void Main()
+            {
+                string s = "Kako je počeo rat na mom otoku?";
+                Console.WriteLine("U '{0}' ima {1} riječi.", s, DajBrojRiječi(s));
 
-            s = "Brojim: jedan, dva, tri! Tko se nije skrio...";
-            Console.WriteLine("U '{0}' ima {1} riječi.", s, DajBrojRiječi(s));
+                s = "Brojim: jedan, dva, tri! Tko se nije skrio...";
+                Console.WriteLine("U '{0}' ima {1} riječi.", s, DajBrojRiječi(s));
+            }
         }
     }
-}
